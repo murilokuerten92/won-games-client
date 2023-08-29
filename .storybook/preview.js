8a88d3395b4909d5f47bac181bd89b7913ea82ed
Murilo Kuerten <murilokuerten@Murilos-MacBook-Pro.local>
2023-08-29 15:15:57 -0300

    feat: Add Home template

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,8 +3,6 @@ import GlobalStyles from '../src/styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from 'styles/theme'
 
-
-
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
