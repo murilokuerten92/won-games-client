1e63491897a22128d03d75196370d90ae538a99b
Murilo Kuerten <murilokuerten@Murilos-MacBook-Pro.local>
2023-09-05 08:07:02 -0300

    feat: Add html and tests to GameInfo

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
 import * as HeadingStyles from 'components/Heading/styles'
-import * as GameCardSliderStyles from 'components/GameCardSlider/styles'
+
 import * as HighlightStyles from 'components/Highlight/styles'
 
 export const SectionBanner = styled.section`
