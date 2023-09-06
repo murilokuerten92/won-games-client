5f5df7b6c9ff338cf26461774b1ae358ec5078f8
Murilo Kuerten <murilokuerten@Murilos-MacBook-Pro.local>
2023-09-06 08:46:20 -0300

    feat: Add Gallery Modal Slider and slickGoTo

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -2,6 +2,8 @@ import SlickSlider, { Settings } from 'react-slick'
 
 import * as S from './styles'
 
+import { forwardRef } from 'react'
+
 export type SliderSettings = Settings
 
 export type SliderProps = {
@@ -9,10 +11,15 @@ export type SliderProps = {
   settings: SliderSettings
 }
 
-const Slider = ({ children, settings }: SliderProps) => (
+const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+  { children, settings },
+  ref
+) => (
   <S.Wrapper>
-    <SlickSlider {...settings}>{children}</SlickSlider>
+    <SlickSlider ref={ref} {...settings}>
+      {children}
+    </SlickSlider>
   </S.Wrapper>
 )
 
-export default Slider
+export default forwardRef(Slider)
