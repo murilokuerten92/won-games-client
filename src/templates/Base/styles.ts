d387d8513533bd4dc4331b4e04f72365cbbb5bb7
Murilo Kuerten <murilokuerten@Murilos-MacBook-Pro.local>
2023-09-12 19:53:16 -0300

    feat: Add Wishlist template and page

--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -15,7 +15,10 @@ export const SectionFooter = styled.section`
   `}
 `
 export const Content = styled.div`
-  flex: 1 0 auto;
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.xlarge};
+    flex: 1 0 auto;
+  `}
 `
 
 export const Wrapper = styled.div`
